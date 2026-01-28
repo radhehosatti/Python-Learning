74633c66fa32d5b69fc53d5d633e9de0e0a275d4
Radhe <radhahosatti@gmail.com>
2026-01-28 15:50:51 +0530

    updated if_while logic

--- basic/if_while.py
+++ basic/if_while.py
@@ -1,10 +1,10 @@
 num = int(input("Enter a number: "))
 
 if num % 2 == 0:
-    print("Even number")
+    print(num,"is Even number")
 else:
-    print("Odd number")
-
+    print(num,"is Odd number")
+print("----------------")
 i = 1
 while i <= num:
     print(i)
