2d7594356f560880dd5ba0bc2c65048674c6246a
Radhe <radhahosatti@gmail.com>
2026-01-29 21:38:06 +0530

    updated for loop examplse

--- basic/for_loop.py
+++ basic/for_loop.py
@@ -7,11 +7,15 @@ print("--------------------")
 
 # Example 2: Print even numbers from 1 to 20
 print("Even numbers from 1 to 20:")
+print("even numbers:")
 for i in range(1, 21):
     if i % 2 == 0:
         print(i)
-
-print("--------------------")
+print("odd numbers:") 
+for i in range(1,21):
+  if i%2!=0:
+     print(i)
+print("--------------------") 
 
 # Example 3: Loop through a string
 name = "Radhe"
