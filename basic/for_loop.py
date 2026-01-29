ccf8d6fae5b291413d7764400cac46c54af7b345
Radhe <radhahosatti@gmail.com>
2026-01-29 21:43:00 +0530

    updated for loop example python

--- basic/for_loop.py
+++ basic/for_loop.py
@@ -7,11 +7,10 @@ print("--------------------")
 
 # Example 2: Print even numbers from 1 to 20
 print("Even numbers from 1 to 20:")
-print("even numbers:")
 for i in range(1, 21):
     if i % 2 == 0:
         print(i)
-print("odd numbers:") 
+print("odd numbers from 1 to 20:") 
 for i in range(1,21):
   if i%2!=0:
      print(i)
