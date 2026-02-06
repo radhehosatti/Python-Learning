da1e70807f4738f823a63f64b8679a8d1be90de1
Radhe <radhahosatti@gmail.com>
2026-02-06 18:41:18 +0530

    added different arrys example

---
+++ basic/ba.py
@@ -0,0 +1,16 @@
+#list
+marks=[10,20,30,40,50]
+marks.append(60)
+print(marks)
+
+#tuple
+point=(10,20,30)
+print(point)
+
+#dictionary
+student={"name":"radha","age":20,"grade":"A"}
+print(student)
+
+#set
+num={1,2,3,4,5,1,3}
+print(num)
